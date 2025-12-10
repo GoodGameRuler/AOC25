82a710f50eda206d75d92f9b1fc841de8c3fec55
Udit <udit.samant@gmail.com>
2025-12-10 16:11:12 +1100

    Init Repo

---
+++ AOC-Rust/d01/src/main.rs
@@ -0,0 +1,11 @@
+pub struct Rotation {
+    pub sign: bool,
+    pub degree: u8,
+}
+
+fn main() {
+    println!("Hello, world!");
+    let dial = 0;
+}
+
+// R, L
--- README.md
+++
@@ -1,4 +0,0 @@
-# Advent of Code 2025
-> Why do advent of code in one language when you can do it many?
-
-Fun stuff coming soon!
